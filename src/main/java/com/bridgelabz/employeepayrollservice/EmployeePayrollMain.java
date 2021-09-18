c49519e51527718b12c2e8a42a2a773dfba5196b
varshini <dreamvarshini@gmail.com>
2021-09-18 14:51:43 +0530

    ADDED: main method to print welcome message

---
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollservice;
+
+public class EmployeePayrollMain 
+{
+
+	public static void main(String[] args) 
+	
+	{
+		System.out.println("----Welcome to employee payroll service----");
+
+	}
+
+}
