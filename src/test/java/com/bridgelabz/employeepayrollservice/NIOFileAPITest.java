5a51b91c73bd66f816bb358389dbf50f122e4e55
varshini <dreamvarshini@gmail.com>
2021-09-18 19:10:07 +0530

    ADDED: method to demontsrate watch service

--- src/test/java/com/bridgelabz/employeepayrollservice/NIOFileAPITest.java
+++ src/test/java/com/bridgelabz/employeepayrollservice/NIOFileAPITest.java
@@ -59,6 +59,16 @@ public class NIOFileAPITest
 		
 	}
 	
+	@Test
+	public void givenAdirectoryWhenWatchedListsAllTheActivities() throws IOException
+	{
+		
+		Path dir =Paths.get(HOME+"/"+PLAY_WITH_NIO);
+		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+		new Java8WatchServiceExample(dir).processEvents();
+		
+	}
+	
 	
 	
 
