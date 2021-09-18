c526ed9785bda5f97dbdc4425b27b5d74629cbf1
varshini <dreamvarshini@gmail.com>
2021-09-19 00:20:16 +0530

    ADDED: test case to check give list written to file

---
+++ src/test/java/com/bridgelabz/employeepayrollservice/EmployeePayrollServiceTest.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.employeepayrollservice;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import com.bridgelabz.employeepayrollservice.EmployeePayrollService.IOService;
+
+import org.junit.Assert;
+
+public class EmployeePayrollServiceTest 
+{
+
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries()
+	{
+		EmployeePayrollData[] arrayOfEmployees= {
+				new EmployeePayrollData(1, "Sheldon", 100000.0),
+				new EmployeePayrollData(2, "Penny",200000.0 ),
+				new EmployeePayrollData(3,"Ted",300000.0)
+				
+		};
+		
+		EmployeePayrollService employeePayrollService;
+		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmployees));
+		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
+		
+		
+	}
+	
+
+	
+}
