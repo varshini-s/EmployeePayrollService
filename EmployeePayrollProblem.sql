965ac6e2b3783f5e530f43647da2159dcf5f37be
varshini-s <53284733+varshini-s@users.noreply.github.com>
2021-09-25 20:54:53 +0530

    ADDED: mysql queries for payroll database

---
+++ EmployeePayrollProblem.sql
@@ -0,0 +1,266 @@
+#UC 1
+CREATE DATABASE payroll_service;
+USE payroll_service;
+SELECT DATABASE();
+
+CREATE TABLE employee_payroll (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(150) NOT NULL,
+    salary DOUBLE NOT NULL,
+    start DATE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+#UC 3
+INSERT INTO employee_payroll(name,salary,start) VALUES
+		('Bill',1000000.00,'2018-01-03'),
+        ('Terisa',2000000.00,'2019-11-29'),
+        ('charlie',300000.00,'2020-05-21');
+        
+#UC 4
+SELECT *
+FROM employee_payroll;
+
+#UC 5
+SELECT salary
+FROM employee_payroll
+WHERE name = 'Bill';
+
+SELECT *
+FROM employee_payroll
+WHERE  start BETWEEN CAST('2018-01-01' AS DATE) AND DATE(NOW());
+
+#UC 6
+ALTER TABLE employee_payroll add gender char(1) after name;
+
+UPDATE employee_payroll 
+SET gender = 'F'
+WHERE name = 'Terisa';
+
+UPDATE employee_payroll 
+SET gender = 'M'
+WHERE name = 'Bill' OR name = 'Charlie';
+    
+UPDATE employee_payroll 
+SET salary = 300000.00
+WHERE name = 'Terisa';
+    
+#UC 7
+SELECT SUM(salary)
+FROM employee_payroll
+GROUP BY gender;
+
+SELECT AVG(salary)
+FROM employee_payroll
+GROUP BY gender;
+
+SELECT COUNT(salary)
+FROM employee_payroll
+GROUP BY gender;
+
+SELECT MIN(salary)
+FROM employee_payroll
+GROUP BY gender;
+
+SELECT MAX(salary)
+FROM employee_payroll
+GROUP BY gender;
+
+#UC 8
+ALTER TABLE employee_payroll add phone_number char(10) after name;
+ALTER TABLE employee_payroll add address varchar(50) after phone_number;
+ALTER TABLE employee_payroll add department varchar(10) not null after address;
+ALTER TABLE employee_payroll ALTER address set default 'ABC';
+
+
+#UC 9
+ALTER TABLE employee_payroll RENAME COLUMN salary TO basic_pay;
+ALTER TABLE employee_payroll ADD deductions DOUBLE NOT NULL AFTER basic_pay;
+ALTER TABLE employee_payroll ADD taxable_pay DOUBLE NOT NULL AFTER deductions;
+ALTER TABLE employee_payroll ADD tax Double NOT NULL AFTER taxable_pay;
+ALTER TABLE employee_payroll ADD net_pay Double NOT NULL AFTER tax;
+
+
+UPDATE employee_payroll 
+SET 
+    phone_number = '9898787856',
+    address = 'ghj',
+    department = 'Marketting',
+    deductions = '10000.00',
+    taxable_pay = '2000.00',
+    tax = '1000.00',
+    net_pay = '3000000.00'
+WHERE id = 1;
+
+UPDATE employee_payroll 
+SET 
+    phone_number = '9778907856',
+    address = 'ABC',
+    deductions = '1000000.00',
+    taxable_pay = '20000000.00',
+    tax = '5000000.00',
+    net_pay = '1500000.00'
+WHERE
+    id = 2;
+
+UPDATE employee_payroll 
+SET 
+    phone_number = '9778907856',
+    address = 'ttt',
+    department = 'IT',
+    deductions = '20000.00',
+    taxable_pay = '1000.00',
+    tax = '3000.00',
+    net_pay = '2000000.00'
+WHERE
+    id = 3;
+
+
+#UC 10
+UPDATE employee_payroll 
+SET department = 'Sales'
+WHERE name = 'Terisa';
+
+INSERT INTO employee_payroll(name,phone_number,department,gender,basic_pay,deductions,taxable_pay,tax,net_pay,start)
+VALUES ('Terisa','9778907856','Marketting','F',3000000.00,1000000.00,20000000.00,5000000.00,1500000.00,'2019-11-29');
+
+
+#UC 11
+ALTER TABLE employee_payroll
+DROP COLUMN address,
+DROP COLUMN department,
+DROP COLUMN basic_pay,
+DROP COLUMN deductions,
+DROP COLUMN taxable_pay,
+DROP COLUMN tax,
+DROP COLUMN net_pay;
+
+
+ALTER TABLE employee_payroll RENAME TO employee;
+
+Delete from employee where id=4;
+
+CREATE TABLE company 
+(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO company(name)
+VALUES ('Reliance'),('Xiaomi');
+
+ALTER TABLE employee add company_id INT UNSIGNED NOT NULL after start;
+
+UPDATE employee 
+SET company_id = 1
+WHERE id=1;
+
+UPDATE employee 
+SET company_id = 2
+WHERE id=2;
+
+UPDATE employee 
+SET company_id = 1
+WHERE id=3;
+
+CREATE TABLE department 
+(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO department(name)
+VALUES ('Marketting'),('Sales'),('IT');
+
+CREATE TABLE payroll 
+(
+    id INT UNSIGNED NOT NULL,
+    basic_pay DOUBLE NOT NULL,
+    deductions DOUBLE NOT NULL,
+    taxable_pay DOUBLE NOT NULL,
+    tax DOUBLE NOT NULL,
+    net_pay DOUBLE NOT NULL,
+    PRIMARY KEY (id),
+	FOREIGN KEY (id) REFERENCES employee(id)
+);
+
+INSERT INTO payroll(id,basic_pay,deductions,taxable_pay,tax,net_pay)
+VALUES (1,1000000,10000.00,2000.00,1000.00,3000000.00)
+	  ,(2,3000000.00,1000000.00,20000000.00,5000000.00,1500000.00)
+      ,(3,1000000,20000.00,1000.00,3000.00,2000000.00);
+
+CREATE TABLE address 
+(
+	id INT UNSIGNED NOT NULL ,
+    house_number VARCHAR(5) NOT NULL,
+	street VARCHAR(20) NOT NULL,
+	city VARCHAR(20) NOT NULL,
+    state VARCHAR(20) NOT NULL,
+    zip VARCHAR(10) NOT NULL,
+    PRIMARY KEY (id),
+	FOREIGN KEY (id) REFERENCES employee(id)
+
+);
+
+INSERT INTO address(id,house_number,street ,city,state,zip)
+VALUES(1,21,'AAA','Bangalore','Karnataka',12345)
+	,(2,1,'tr','Mysore','Karnataka',45333)
+	,(3,33,'green','Bangalore','Karnataka',23455);
+
+
+CREATE TABLE employee_department 
+(
+    employee_id INT UNSIGNED NOT NULL,
+    department_id INT UNSIGNED NOT NULL,
+	FOREIGN KEY (department_id) REFERENCES department(id),
+    FOREIGN KEY (employee_id) REFERENCES employee(id)
+
+);
+
+INSERT INTO employee_department(employee_id,department_id)
+VALUES (1,2),(2,1),(2,2),(3,3);
+
+#UC 12
+SELECT *
+FROM payroll
+WHERE id = (SELECT id
+            FROM employee
+            WHERE name = 'Bill');
+            
+SELECT *
+FROM employee
+WHERE  start BETWEEN CAST('2018-01-01' AS DATE) AND DATE(NOW()); 
+            
+
+SELECT SUM(basic_pay),gender  
+FROM payroll   
+JOIN employee  
+ON payroll.id=employee.id
+GROUP BY gender;
+
+SELECT AVG(basic_pay),gender  
+FROM payroll   
+JOIN employee  
+ON payroll.id=employee.id
+GROUP BY gender;
+
+SELECT COUNT(basic_pay),gender  
+FROM payroll   
+JOIN employee  
+ON payroll.id=employee.id
+GROUP BY gender;
+
+SELECT MIN(basic_pay),gender  
+FROM payroll   
+JOIN employee  
+ON payroll.id=employee.id
+GROUP BY gender;
+
+SELECT MAX(basic_pay),gender  
+FROM payroll   
+JOIN employee  
+ON payroll.id=employee.id
+GROUP BY gender;
+
