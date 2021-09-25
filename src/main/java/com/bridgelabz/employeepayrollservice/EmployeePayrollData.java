7bc41cf26ae964d890b65aea687498fd0223b85c
varshini <dreamvarshini@gmail.com>
2021-09-25 23:26:36 +0530

    ADDED: constructor to add employee salary

--- src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollData.java
@@ -1,10 +1,13 @@
 package com.bridgelabz.employeepayrollservice;
 
+import java.time.LocalDate;
+
 public class EmployeePayrollData 
 {
 	public int id;
 	public String employeeName;
 	public double salary;
+	public LocalDate startDate;
 	
 	public EmployeePayrollData(int id,String employeeName,double salary) 
 	{
@@ -13,6 +16,13 @@ public class EmployeePayrollData
 		this.salary=salary;
 	}
 	
+	public EmployeePayrollData(int id, String employeeName, double salary, LocalDate startDate)
+	{
+		this(id, employeeName, salary);
+		this.startDate=startDate;
+		
+	}
+
 	public String toString()
 	{
 		return "id="+id+",name='"+employeeName+'\''+", salary="+salary;
