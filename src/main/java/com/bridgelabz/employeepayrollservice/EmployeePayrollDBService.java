b00df40843b69bdc0cabc51fb0ac8e442152873d
varshini <dreamvarshini@gmail.com>
2021-09-25 23:15:25 +0530

    ADDED: function to establish database connection

---
+++ src/main/java/com/bridgelabz/employeepayrollservice/EmployeePayrollDBService.java
@@ -0,0 +1,35 @@
+package com.bridgelabz.employeepayrollservice;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.sql.Connection;
+
+public class EmployeePayrollDBService 
+{
+
+	private Connection getConnection() throws SQLException 
+	{
+		String jdbcURL ="jdbc:mysql://localhost:3306/payroll_service?allowPublicKeyRetrieval=true&useSSL=false";
+		String username="root";
+		String password="Demo@1234";
+		Connection connection = null;
+		System.out.println("Connecting to database"+jdbcURL);
+		connection=DriverManager.getConnection(jdbcURL,username,password);
+		System.out.println("Connection is successfull!!"+connection);
+
+
+		return connection;
+	}
+
+	
+
+
+
+}
